d29739f43653e11a3d0b30d97122b520a8c4dfa3
Fepozopo <fepozopo.f39@gmail.com>
2024-12-20 15:41:39 -0800

    Updated the exit command to print a message before exiting

--- command_exit.go
+++ command_exit.go
@@ -1,9 +1,13 @@
 package main
 
-import "os"
+import (
+	"fmt"
+	"os"
+)
 
 // commandExit exits the PokedexCLI application.
 func commandExit(cfg *config, args ...string) error {
+	fmt.Printf("Closing the Pokedex... Goodbye!\n")
 	os.Exit(0)
 	return nil
 }
